7fe43ba22617ea4c9e42f0f5ef43698f45bde927
David Restrepo Zuluaga <davidrestrepozu@gmail.com>
2022-05-20 08:00:17 -0500

    Create controller and change  data types in model

--- RamsayTestSolution/Ramsay.Models/Student.cs
+++ RamsayTestSolution/Ramsay.Models/Student.cs
@@ -3,10 +3,10 @@
     public class Student
     {
         public int Id { get; set; }
-        public int UserName { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
-        public int Career { get; set; }
+        public string Career { get; set; }
     }
 }
---
+++ RamsayTestSolution/Ramsay.WebApi/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ramsay.Models;
+using Ramsay.UnitOfWork;
+using System;
+
+namespace Ramsay.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        protected IUnitOfWork _unitOfWork;
+        public StudentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            try
+            {
+                var response = _unitOfWork.Student.GetAll();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateStudent")]
+        public IActionResult CreateStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var response = _unitOfWork.Student.Insert(student);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("UpdateStudent")]
+        public IActionResult UpdateStudent([FromBody] Student student)
+        {
+            if (ModelState.IsValid && _unitOfWork.Student.Update(student))
+            {
+                return Ok(new { Message = "The Student is updated" });
+            }
+
+            return BadRequest(new { Message = "The student could not be updated" });
+        }
+
+        [HttpDelete]
+        [Route("DeleteStudent")]
+        public IActionResult DeleteStudent([FromBody] Student student)
+        {
+            if (student.Id > 0)
+            {
+                _unitOfWork.Student.Delete(student);
+                return Ok(new { Message = "The Student is Deleted" });
+            }
+
+            return BadRequest(new { Message = "The student could not be removed" });
+        }
+    }
+}
